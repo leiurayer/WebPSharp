75927ef423bf61384fc113fb855cc2e78ddbaf2c
leiurayer <1432593898@qq.com>
2022-04-05 17:33:13 +0800

    添加项目文件。

---
+++ WebP.Net/Exception/WebPException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace WebP.Net.Exception
+{
+    public class WebPException : System.Exception
+    {
+        public WebPException() : base()
+        {
+        }
+
+        public WebPException(string message) : base(message)
+        {
+        }
+
+        public WebPException(string message, System.Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected WebPException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ WebP.Net/Interop/Delegate64.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using WebP.Net.Enum;
+using WebP.Net.Struct;
+
+namespace WebP.Net.Interop
+{
+    internal class Delegate64
+    {
+        #region VP8 decode
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate int VP8CheckSignature([MarshalAs(UnmanagedType.LPArray)] byte[] data, UInt64 data_size);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate int VP8GetInfo([MarshalAs(UnmanagedType.LPArray)] byte[] data, UInt64 data_size, UInt64 chunk_size, ref int width, ref int height);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate int VP8LCheckSignature([MarshalAs(UnmanagedType.LPArray)] byte[] data, UInt64 data_size);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate int VP8LGetInfo([MarshalAs(UnmanagedType.LPArray)] byte[] data, UInt64 data_size, ref int width, ref int height, ref int has_alpha);
+
+        #endregion
+
+        #region VP8 dsp
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate int VP8GetCPUInfo(CPUFeature feature);
+        #endregion
+
+        #region WebP encode
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        internal delegate void WebPBlendAlpha(ref WebPPicture pic, uint background_rgb);
+
+
+        #endregion
+
+
+    }
+}
