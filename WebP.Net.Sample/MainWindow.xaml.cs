93255e8f37944a112b13e6c82f789b99c1ebee78
leiurayer <1432593898@qq.com>
2022-04-27 22:42:47 +0800

    update SimpleDecoder

--- WebP.Net.Sample/MainWindow.xaml.cs
+++ WebP.Net.Sample/MainWindow.xaml.cs
@@ -23,57 +23,56 @@ namespace WebP.Net.Sample
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            byte[] bytes = File.ReadAllBytes("Gallery/WebP Gallery/1.webp");
+            //byte[] bytes = File.ReadAllBytes("Gallery/WebP Gallery/1.webp");
             //byte[] bytes = File.ReadAllBytes("Gallery/Lossless and Alpha Gallery/1_webp_a.webp");
-            IntPtr data = Marshal.UnsafeAddrOfPinnedArrayElement(bytes, 0);
-            WebPBitstreamFeatures features = new WebPBitstreamFeatures();
+            //IntPtr data = Marshal.UnsafeAddrOfPinnedArrayElement(bytes, 0);
+            //WebPBitstreamFeatures features = new WebPBitstreamFeatures();
 
-            VP8StatusCode code = LibWebP.WebPGetFeatures(data, (uint)bytes.LongLength, ref features);
-            if (code != VP8StatusCode.VP8_STATUS_OK)
-            {
-                return;
-            }
+            //VP8StatusCode code = LibWebP.WebPGetFeatures(data, (uint)bytes.LongLength, ref features);
+            //if (code != VP8StatusCode.VP8_STATUS_OK)
+            //{
+            //    return;
+            //}
 
-            int width = features.Width;
-            int height = features.Height;
-            Bitmap bitmap;
-            BitmapData bitmapData;
+            //int width = features.Width;
+            //int height = features.Height;
+            //Bitmap bitmap;
+            //BitmapData bitmapData;
 
-            if (features.HasAlpha == 0)
-            {
-                //Allocate canvas
-                bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
-                //Lock surface for writing
-                bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            //if (features.HasAlpha == 0)
+            //{
+            //    //Allocate canvas
+            //    bitmap = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+            //    //Lock surface for writing
+            //    bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
 
-                IntPtr info = LibWebP.WebPDecodeBGRInto(data, (uint)bytes.LongLength, bitmapData.Scan0, (uint)(bitmapData.Stride * bitmapData.Height), bitmapData.Stride);
-                if (bitmapData.Scan0 != info)
-                {
-                    throw new System.Exception("Failed to decode WebP image with error " + (long)info);
-                }
-            }
-            else
-            {
-                //Allocate canvas
-                bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-                //Lock surface for writing
-                bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
+            //    IntPtr info = LibWebP.WebPDecodeBGRInto(data, (uint)bytes.LongLength, bitmapData.Scan0, (uint)(bitmapData.Stride * bitmapData.Height), bitmapData.Stride);
+            //    if (bitmapData.Scan0 != info)
+            //    {
+            //        throw new System.Exception("Failed to decode WebP image with error " + (long)info);
+            //    }
+            //}
+            //else
+            //{
+            //    //Allocate canvas
+            //    bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            //    //Lock surface for writing
+            //    bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format32bppArgb);
 
-                IntPtr info = LibWebP.WebPDecodeBGRAInto(data, (uint)bytes.LongLength, bitmapData.Scan0, (uint)(bitmapData.Stride * bitmapData.Height), bitmapData.Stride);
-                if (bitmapData.Scan0 != info)
-                {
-                    throw new WebPException("Failed to decode WebP image with error " + (long)info);
-                }
-            }
+            //    IntPtr info = LibWebP.WebPDecodeBGRAInto(data, (uint)bytes.LongLength, bitmapData.Scan0, (uint)(bitmapData.Stride * bitmapData.Height), bitmapData.Stride);
+            //    if (bitmapData.Scan0 != info)
+            //    {
+            //        throw new WebPException("Failed to decode WebP image with error " + (long)info);
+            //    }
+            //}
 
-            //Unlock surface
-            if (bitmapData != null && bitmap != null) bitmap.UnlockBits(bitmapData);
-            //Dispose of bitmap if anything went wrong
-            //if (bitmap != null) bitmap.Dispose();
+            string img = "Gallery/WebP Gallery/1.webp";
+            SimpleDecoder simpleDecoder = new SimpleDecoder(img);
+            Bitmap bitmap = simpleDecoder.WebPtoBitmap();
 
             nameTestWebP.Source = BitmapToBitmapImage(bitmap, ImageFormat.Png);
-            nameTestWebP.Width = width;
-            nameTestWebP.Height = height;
+            //nameTestWebP.Width = width;
+            //nameTestWebP.Height = height;
 
             bitmap.Save("test.png", ImageFormat.Png);
         }
---
+++ WebP.Net.Test/TestSimpleDecoder.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Drawing;
+using WebP.Net.Struct;
+
+namespace WebP.Net.Test
+{
+    [TestClass]
+    public class TestSimpleDecoder
+    {
+        private readonly List<Image> ImageList = new List<Image>
+        {
+            new Image { Name = "Gallery/WebP Gallery/1.webp", Width = 550, Height = 368, HasAlpha = 0, HasAnimation = 0, Format = 1 },
+            new Image { Name = "Gallery/WebP Gallery/2.webp", Width = 550, Height = 404, HasAlpha = 0, HasAnimation = 0, Format = 1 },
+            new Image { Name = "Gallery/WebP Gallery/3.webp", Width = 1280, Height = 720, HasAlpha = 0, HasAnimation = 0, Format = 1 },
+            new Image { Name = "Gallery/WebP Gallery/4.webp", Width = 1024, Height = 772, HasAlpha = 0, HasAnimation = 0, Format = 1 },
+            new Image { Name = "Gallery/WebP Gallery/5.webp", Width = 1024, Height = 752, HasAlpha = 0, HasAnimation = 0, Format = 1 },
+
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/1_webp_ll.webp", Width = 400, Height = 301, HasAlpha = 1, HasAnimation = 0, Format = 2 },
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/1_webp_a.webp", Width = 400, Height = 301, HasAlpha = 1, HasAnimation = 0, Format = 1  },
+
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/2_webp_ll.webp", Width = 386, Height = 395, HasAlpha = 1, HasAnimation = 0, Format = 2  },
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/2_webp_a.webp", Width = 386, Height = 395, HasAlpha = 1, HasAnimation = 0, Format = 1  },
+
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/3_webp_ll.webp", Width = 800, Height = 600, HasAlpha = 1, HasAnimation = 0, Format = 2  },
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/3_webp_a.webp", Width = 800, Height = 600, HasAlpha = 1, HasAnimation = 0, Format = 1  },
+
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/4_webp_ll.webp", Width = 421, Height = 163, HasAlpha = 1, HasAnimation = 0, Format = 2  },
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/4_webp_a.webp", Width = 421, Height = 163, HasAlpha = 1, HasAnimation = 0, Format = 1  },
+
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/5_webp_ll.webp", Width = 300, Height = 300, HasAlpha = 1, HasAnimation = 0, Format = 2  },
+            new Image { Name = "Gallery/Lossless and Alpha Gallery/5_webp_a.webp", Width = 300, Height = 300, HasAlpha = 1, HasAnimation = 0, Format = 1  },
+        };
+
+        [TestMethod]
+        public void TestWebPtoBitmap()
+        {
+            foreach (var item in ImageList)
+            {
+                SimpleDecoder simpleDecoder = new SimpleDecoder(item.Name);
+                Bitmap bitmap = simpleDecoder.WebPtoBitmap();
+
+                WebPBitstreamFeatures features = new WebPBitstreamFeatures();
+                bitmap = simpleDecoder.WebPtoBitmap(ref features);
+
+                //Assert.AreEqual(1, info);
+                Assert.AreEqual(item.Width, features.Width);
+                Assert.AreEqual(item.Height, features.Height);
+            }
+
+
+        }
+    }
+}
